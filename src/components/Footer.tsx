fa9c44c16eb4449645863f1c829f1fb7c036055f
yogimp <yogimeizalputra@gmail.com>
2023-08-25 12:49:41 +0700

    Adding Footer Components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between'>
+            <Link href='/' className='font-bold text-xl'>PEPENERO</Link>
+            <p>© ALL RIGHT RESERVERD.</p>
         </div>
     )
 }
