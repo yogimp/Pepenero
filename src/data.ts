40cba4b60f1106eac079b7d2e720c852c3838426
yogimp <yogimeizalputra@gmail.com>
2023-08-27 16:34:37 +0700

    Adding Category page

--- src/data.ts
+++ src/data.ts
@@ -374,7 +374,7 @@ export const menu: Menu = [
     id: 2,
     slug: "burgers",
     title: "Juicy Burgers",
-    desc: "Burger Bliss: Juicy patties, bold flavors, and gourmet toppings galore.",
+    desc: "Burger Bliss: Juicy patties, bold flavors, and gourmet toppings galore hot sweet sauce.",
     img: "/temporary/m2.png",
     color: "black",
 },
