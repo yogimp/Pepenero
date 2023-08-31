391560651230c4e41d6220836298357b402222fb
yogimp <yogimeizalputra@gmail.com>
2023-08-31 12:38:29 +0700

    Adding Products List

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -7,9 +7,7 @@ const endingDate = new Date('2023-10-11')
 
 const CountDown = () => {
     return (
-        <div>
-            <Countdown date={endingDate} className='font-bold text-5xl text-yellow-300'/>
-        </div>
+        <Countdown date={endingDate} className='font-bold text-5xl text-yellow-300' />
     )
 }
 
