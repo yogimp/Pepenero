bfe6805f2158da0364762a68276fe60c34cd008a
yogimp <yogimeizalputra@gmail.com>
2023-09-01 15:48:13 +0700

    Adding Orders Page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between'>
+        <div className='h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between'>
             <Link href='/' className='font-bold text-xl'>PEPENERO</Link>
             <p>© ALL RIGHT RESERVERD.</p>
         </div>
--- src/data.ts
+++ src/data.ts
@@ -362,28 +362,28 @@ type Menu = {
 }[];
 
 export const menu: Menu = [
-{
-    id: 1,
-    slug: "pastas",
-    title: "Italian Pastas",
-    desc: "Savor the taste of perfection with our exquisite Italian handmade pasta menu.",
-    img: "/temporary/m1.png",
-    color: "white",
-},
-{
-    id: 2,
-    slug: "burgers",
-    title: "Juicy Burgers",
-    desc: "Burger Bliss: Juicy patties, bold flavors, and gourmet toppings galore hot sweet sauce.",
-    img: "/temporary/m2.png",
-    color: "black",
-},
-{
-    id: 3,
-    slug: "pizzas",
-    title: "Cheesy Pizzas",
-    desc: "Pizza Paradise: Irresistible slices, mouthwatering toppings, and cheesy perfection.",
-    img: "/temporary/m3.png",
-    color: "white",
-},
+    {
+        id: 1,
+        slug: "pastas",
+        title: "Italian Pastas",
+        desc: "Savor the taste of perfection with our exquisite Italian handmade pasta menu.",
+        img: "/temporary/m1.png",
+        color: "white",
+    },
+    {
+        id: 2,
+        slug: "burgers",
+        title: "Juicy Burgers",
+        desc: "Burger Bliss: Juicy patties, bold flavors, and gourmet toppings galore hot sweet sauce.",
+        img: "/temporary/m2.png",
+        color: "black",
+    },
+    {
+        id: 3,
+        slug: "pizzas",
+        title: "Cheesy Pizzas",
+        desc: "Pizza Paradise: Irresistible slices, mouthwatering toppings, and cheesy perfection.",
+        img: "/temporary/m3.png",
+        color: "white",
+    },
 ];
